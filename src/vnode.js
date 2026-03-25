d13e31be06126dc2003b2cb3c670e7b34581fbc2
sijun-yang <yangsijun5528@gmail.com>
2026-03-25 18:56:52 +0900

    Remove redundant vnode module

--- src/vnode.js
+++
@@ -1,9 +0,0 @@
-import { NodeType } from "./constants.js";
-
-export function textNode(value) {
-  return { nodeType: NodeType.TEXT, value };
-}
-
-export function elementNode(type, props = {}, children = []) {
-  return { nodeType: NodeType.ELEMENT, type, props, children };
-}
